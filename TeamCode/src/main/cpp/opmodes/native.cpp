4dcf69924cf07d5173971b29279535b8f57e26c2
z3db0y <us3r0002@gmail.com>
2024-12-19 22:25:48 +0200

    add crashpad

--- TeamCode/src/main/cpp/opmodes/native.cpp
+++ TeamCode/src/main/cpp/opmodes/native.cpp
@@ -26,6 +26,10 @@ void Native::runOpMode() {
 extern "C"
 JNIEXPORT void JNICALL
 Java_org_firstinspires_ftc_teamcode_test_Native_runOpMode(JNIEnv *p_jni, jobject self) {
-    auto *opMode = new Native(p_jni, self);
-    opMode->runOpMode();
+    try {
+        auto *opMode = new Native(p_jni, self);
+        opMode->runOpMode();
+    } catch (std::exception& e) {
+        p_jni->ThrowNew(p_jni->FindClass("java/lang/Exception"), e.what());
+    }
 }
